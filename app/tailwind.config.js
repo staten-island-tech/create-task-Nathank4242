a7e54fb744f6c3f0939451dacb3f7f1f03bb1d40
Nathank4242 <nkweon@gmail.com>
2024-12-31 12:58:35 -0500

    create

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./index.html", "./js/main.js", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
