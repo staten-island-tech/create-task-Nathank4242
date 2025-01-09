68abd254e100029e4f3e942614f56229adbdc5d1
Nathank4242 <nkweon@gmail.com>
2025-01-09 13:59:11 -0500

    create

--- app/js/main.js
+++ app/js/main.js
@@ -78,10 +78,10 @@ function createCards(states) {
   states.forEach((state) => {
     const cardHTML = `
         <div class="card bg-black text-white rounded-lg shadow-2xl p-6 text-center border-gray-300">
-        <h2 class="card-name text-2xl font-semibold"> Capital:State:</h2>
+        <h2 class="card-name text-2xl font-semibold"> State:</h2>
           <h2 class="card-name text-2xl font-semibold"> Capital: ${state.capital}</h2>
           <h2 class="card-name text-2xl font-semibold"> State population: ${state.population}</h2>
-          <h2 class="card-name text-2xl font-semibold"> Year Establish: ${state.yearEstablished}</h2>
+          <h2 class="card-name text-2xl font-semibold"> Year Established: ${state.yearEstablished}</h2>
         </div>
       `;
     container.insertAdjacentHTML("beforeend", cardHTML);
